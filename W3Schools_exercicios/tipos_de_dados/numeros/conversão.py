2235bad9900ed4d630d39ea39cfad552f0deff28
Mishy-Boog <mah.hil@outlook.com>
2025-03-12 23:36:39 -0300

    Create conversão.py

---
+++ W3Schools_exercicios/tipos_de_dados/numeros/conversão.py
@@ -0,0 +1,25 @@
+#conversão
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#converte int para float:
+a = float(x)
+
+#converte float para int:
+b = int(y)
+
+#converte int para complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#numeros complexos nao podem ser convertidos
+
