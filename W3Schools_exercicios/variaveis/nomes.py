f6c6ae1533ebc32c88eee47068aa263bc802d0b7
Mishy-Boog <mah.hil@outlook.com>
2025-03-12 21:59:20 -0300

    Create nomes.py

---
+++ W3Schools_exercicios/variaveis/nomes.py
@@ -0,0 +1,26 @@
+#não pode ter espaço, começar com numero ou ter algum caractere especial diferente de "_"
+
+#nomes aceitos
+myvar = "a"
+my_var = "a"
+_my_var = "a"
+myVar = "a"
+MYVAR = "a"
+myvar2 = "a"
+
+#nomes recusados
+2myvar = "a"
+my-var = "a"
+my var = "a"
+
+
+Camel Case #geralmente usado no java
+myVariableName = "a"
+
+
+Pascal Case
+MyVariableName = "a"
+
+
+Snake Case #geralmente usado no python
+my_variable_name = "a"
