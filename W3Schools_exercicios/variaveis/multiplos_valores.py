8164b54f33ed9e4571d823485e0816952853d0dd
Mishy-Boog <mah.hil@outlook.com>
2025-03-12 22:03:42 -0300

    Create multiplos_valores.py

---
+++ W3Schools_exercicios/variaveis/multiplos_valores.py
@@ -0,0 +1,42 @@
+x, y, z = 'Uva', 'Banana', 'Caju'
+
+print(x, y, z)
+
+'''
+saída = Uva Banana Caju
+atribuição de vários valores em uma só linha
+'''
+
+###############################
+x = y = z = 'Laranja'
+print(x)
+print(y)
+print(z)
+
+'''
+saída = Laranja
+        Laranja
+        Laranja
+É possivel atribuir o mesmo valor a atributos diferentes
+'''
+###############################################################3
+frutas = ["Maçã", "Banana", "Uva"] #lista
+x, y, z = frutas
+print(x)
+print(y)
+print(z)
+
+pessoas = ('Fulano', 'Ciclano', 'Beltrano') #tupla
+a, b, c = pessoas
+print(a)
+print(b)
+print(c)
+'''
+saída = Maçã
+        Banana
+        Uva
+        Fulano
+        Ciclano
+        Beltrano
+É possivel retirar atributos de listas e tuplas
+'''
