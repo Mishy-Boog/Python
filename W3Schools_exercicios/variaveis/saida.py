e5782ac5023099a19f3e7f33a3620cac2d26be3d
Mishy-Boog <mah.hil@outlook.com>
2025-03-12 22:07:50 -0300

    Create saida.py

---
+++ W3Schools_exercicios/variaveis/saida.py
@@ -0,0 +1,21 @@
+a = 'Estou com fome'
+print(a)
+
+b = 'Estou'
+c = 'com'
+d = 'sede'
+print(b, c, d)
+
+e = 'Estou '
+f = 'com '
+g = 'sono'
+
+print(e + f + g)
+#o uso de "+" sem dar espaço deixa tudo grudado
+'''
+O "+" só deve ser usado com o mesmo tipo (ex: str + str), 
+caso contrario, utilize virgula
+'''
+i = 9
+j = 40
+print(i + j)
