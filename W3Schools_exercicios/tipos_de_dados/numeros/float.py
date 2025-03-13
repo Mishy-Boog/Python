33a286e58b28432e19bb63400aa97b984317a32d
Mishy-Boog <mah.hil@outlook.com>
2025-03-12 23:35:00 -0300

    Update float.py

--- W3Schools_exercicios/tipos_de_dados/numeros/float.py
+++ W3Schools_exercicios/tipos_de_dados/numeros/float.py
@@ -7,3 +7,14 @@ z = -67.15
 print(type(x))
 print(type(y))
 print(type(z))
+
+########################################################
+
+#'e' indicando a potencia de 10
+x = 35e3 #(35x10^3)
+y = 12E4 #(12x10^4)
+z = -87.7e100 #(-87.7x10^100)
+
+print(type(x))
+print(type(y))
+print(type(z))
