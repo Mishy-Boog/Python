02d83b0fa6ac507776cec6870e03b53ac5b1996a
Mishy-Boog <mah.hil@outlook.com>
2025-03-13 12:30:04 -0300

    Create basico_de_strings.py

---
+++ W3Schools_exercicios/strings/basico_de_strings.py
@@ -0,0 +1,63 @@
+print("Oiii")
+print('Oiii')
+#as duas dão o mesmo retorno, tanto faz o tipo de aspas utilizado
+
+###################################################################
+
+print('Oii, você já ouviu falar do "Bob"?')
+print("Você já tomou 'Ades'?")
+
+#É possivel usar aspas dentro da propria str desde que sejam de outro tipo
+
+############################################################################
+
+a = 'Uva'
+print(x)
+#string atribuida a uma variavel
+
+#############################################################################
+
+a = """Se você não gosta do seu destino,não o aceite. 
+Em vez disso, tenha a coragem para transformá-lo
+naquilo que você quer que ele seja.
+    -Naruto """
+print(a)
+
+b = '''Você não é derrotado quando perde, 
+mas sim quando você desiste.
+    -Dragon Ball Z'''
+print(b)
+
+#É possivel fazer blocos de texto usando aspas
+
+#######################################################################
+
+a = 'Mas que calor!'
+print(a[1])
+
+#É possivel imprimir só uma parte da str pelo uso do indice
+
+###########################################################################
+
+for x in 'Banana':
+  print(x)
+
+#É possivel fazer um looping com str, assim ele percorre as letras
+
+###########################################################################
+
+a = 'Como o dia está ensolarado!'
+print(len(a))
+
+#O 'len' conta o tamanho total da str, incluindo todos os caracteres
+
+
+
+
+
+
+
+
+
+
+
